c2979c787d9bde42801425f21fa030bdedb2cddd
arasan01 <developer@arasan01.dev>
2022-05-03 21:26:33 +0900

    Initial Commit

---
+++ Shared/ProgrammableCalenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgrammableCalenderApp.swift
+//  Shared
+//
+//  Created by 新山響生 on 2022/05/03.
+//
+
+import SwiftUI
+
+@main
+struct ProgrammableCalenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
